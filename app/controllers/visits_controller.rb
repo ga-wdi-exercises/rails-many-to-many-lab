e5d51cf16a63b6eb00a6f4cfb30f748decd53266
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-11-10 17:40:46 -0500

    completes lab

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,5 +1,5 @@
 class VisitsController < ApplicationController
-  before_action :get_visit, only: [:show, :create, :edit, :update]
+  before_action :get_visit, only: [:show, :edit, :update, :destroy]
 
   def get_visit
     @visit = Visit.find(params[:id])
@@ -14,20 +14,30 @@ class VisitsController < ApplicationController
 
   def new
     @visit = Visit.new
+    @docs = Doctor.all.map{|d| [d.name, d.id]}
+    @pats = Patient.all.map{|p| [p.name, p.id]}
   end
 
   def create
-    @visit = Visit.create!(visit_params)
+    Visit.create!(visit_params)
+    # render plain: params.inspect
     redirect_to visits_path
   end
 
   def edit
+    @docs = Doctor.all.map{|d| [d.name, d.id]}
+    @pats = Patient.all.map{|p| [p.name, p.id]}
   end
 
   def update
+    @visit.update(visit_params)
+    redirect_to visit_path
   end
 
   def destroy
+    @visit.destroy
+
+    redirect_to visits_path
   end
 
 private
