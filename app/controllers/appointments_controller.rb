17aff0868a034b062a9f95ad51b493f014d0d7b5
robert <robert.k25@gmail.com>
2016-11-10 20:28:26 -0500

    adds new view and create action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,16 +1,32 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments =  Appointment.all
   end
 
   def show
+    @appointment = Appointment.find(params[:id])
   end
 
   def new
+    #@doctors =  Doctor.all.map { |doctor| [doctor.id, doctor.name]  }
+    @doctors = Doctor.all
+    @patients =  Patient.all
+    @appointment = Appointment.new
   end
-  
+
   def create
+    @appointment = Appointment.create(appointment_params)
+    @appointment.save
+    redirect_to appointment_path(@appointment)
+
   end
 
   def edit
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:doctor_id, :patient_id)
   end
 end
